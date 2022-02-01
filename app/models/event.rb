c42e5c67f4eea5c1948185d0f34e4457dda9d416
Jair Ramirez <cyberal159@gmail.com>
2022-02-01 17:14:10 -0500

    validaciones, asociaciones

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,9 @@
 class Event < ApplicationRecord
   belongs_to :user
+  has_many :booking_requests
+
+  validates :name, presence: true, { minimum: 6 }
+  validates :modality, presence: true, inclusion: { in: %w(virtual presencial) }
+  validates :date, presence: true
+  validates :description, presence: true, length: { maximum: 500 }
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :career
+
+  validates :content, presence: true
+  validates :rating, presence: true, inclusion: { 1..10 }
+
 end
