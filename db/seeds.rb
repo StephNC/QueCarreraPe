3302676d6dac3a5d54e9cf8571bbabd352c2e518
Stephanie Nuñez <nunezcorrea.stephanie@gmail.com>
2022-02-02 14:47:31 -0500

    add career controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Creado carreras"
+Career.create(name: "Ingeniería Industrial", area: "Ingeniería", duration: 5, profile: "Mejora de procesos", description: "abc")
+Career.create(name: "Medicina", area: "Ingeniería", duration: 7, profile: "abc", description: "abc")
+Career.create(name: "Administración", area: "Ingeniería", duration: 5, profile: "abc", description: "abc")
+Career.create(name: "Derecho", area: "Ingeniería", duration: 6, profile: "abc", description: "abc")
+Career.create(name: "Arquitectura", area: "Ingeniería", duration: 5, profile: "abc", description: "abc")
