018bc313a0030f367aa8787d302adc1a720a91b0
angela cuadros <angelascc16@gmail.com>
2022-02-03 18:23:46 -0500

    aupdate authentication

--- app/controllers/careers_controller.rb
+++ app/controllers/careers_controller.rb
@@ -1,4 +1,7 @@
 class CareersController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
+
   before_action :set_career, only: [ :show, :edit, :update, :destroy ]
 
   def index
