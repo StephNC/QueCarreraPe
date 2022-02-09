1217ec0b17038bdd0430685758da63cefed1ae85
angela cuadros <angelascc16@gmail.com>
2022-02-09 17:50:30 -0500

    add modal test

--- app/javascript/form/test.js
+++ app/javascript/form/test.js
@@ -1,52 +1,70 @@
 const testFunction = () => {
-  const form = document.getElementById("form");
+  if (window.location.href.includes('test')) {
+    const form = document.getElementById("form");
 
-  form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const c = document.querySelectorAll(".C");
-    const h = document.querySelectorAll(".H");
-    const a = document.querySelectorAll(".A");
-    const s = document.querySelectorAll(".S");
-    const i = document.querySelectorAll(".I");
-    const d = document.querySelectorAll(".D");
-    const e = document.querySelectorAll(".E");
-    console.log(h);
-    const sum_c = area(c);
-    const sum_h = area(h);
-    const sum_a = area(a);
-    const sum_s = area(s);
-    const sum_i = area(i);
-    const sum_d = area(d);
-    const sum_e = area(e);
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const modal = document.querySelector("#results");
 
-    console.log(sum_c, "resultado c");
-    console.log(sum_h, "resultado h");
-    console.log(sum_a, "resultado a");
+      const preguntasNoContestadas = [];
 
-    const max = Math.max(sum_c, sum_h, sum_a, sum_s, sum_i, sum_d, sum_e);
-    console.log(max);
+      const c = document.querySelectorAll(".C");
+      const h = document.querySelectorAll(".H");
+      const a = document.querySelectorAll(".A");
+      const s = document.querySelectorAll(".S");
+      const i = document.querySelectorAll(".I");
+      const d = document.querySelectorAll(".D");
+      const e = document.querySelectorAll(".E");
+      console.log(h);
+      const sum_c = area(c, preguntasNoContestadas);
+      const sum_h = area(h, preguntasNoContestadas);
+      const sum_a = area(a, preguntasNoContestadas);
+      const sum_s = area(s, preguntasNoContestadas);
+      const sum_i = area(i, preguntasNoContestadas);
+      const sum_d = area(d, preguntasNoContestadas);
+      const sum_e = area(e, preguntasNoContestadas);
 
-    const areas = [
-      { area: 'ADMINISTRATIVAS Y CONTABLES', quantity: sum_c },
-      { area: 'HUMANISTA Y SOCIALES', quantity: sum_h },
-      { area: 'ARTISTICAS', quantity: sum_a },
-      { area: 'MEDICINA Y CIENCIAS DE LA SALUD', quantity: sum_s },
-      { area: 'INGENIERIA Y COMPUTACION', quantity: sum_i },
-      { area: 'DEFENSA Y SEGURIDAD ', quantity: sum_d },
-      { area: 'CIENCIAS EXACTAS Y AGRARIAS', quantity: sum_e }
-    ];
+      if (preguntasNoContestadas.length > 0) {
+        modal.querySelector(".modal-body").innerHTML = `Falta completar la pregunta ${preguntasNoContestadas.join(", ").replace(/question/g, "")}`;
+        modal.querySelector("#go-to-career").style.display = 'none';
+        return;
+      }
 
-    const finalResult = areas.find(quant => quant.quantity === max);
-    console.log(finalResult.area)
+      console.log(sum_c, "resultado c");
+      console.log(sum_h, "resultado h");
+      console.log(sum_a, "resultado a");
 
-    window.location.replace(`careers?query=${finalResult.area}`);
+      const max = Math.max(sum_c, sum_h, sum_a, sum_s, sum_i, sum_d, sum_e);
+      console.log(max);
 
-  });
+      const areas = [
+        { area: 'ADMINISTRATIVAS Y CONTABLES', quantity: sum_c },
+        { area: 'HUMANISTA Y SOCIALES', quantity: sum_h },
+        { area: 'ARTISTICAS', quantity: sum_a },
+        { area: 'MEDICINA Y CIENCIAS DE LA SALUD', quantity: sum_s },
+        { area: 'INGENIERIA Y COMPUTACION', quantity: sum_i },
+        { area: 'DEFENSA Y SEGURIDAD ', quantity: sum_d },
+        { area: 'CIENCIAS EXACTAS Y AGRARIAS', quantity: sum_e }
+      ];
+
+      const finalResult = areas.find(quant => quant.quantity === max);
+      console.log(finalResult.area)
+
+      modal.querySelector(".modal-body").innerHTML = `El área que tienes mayor afinidad es ${finalResult.area}`;
+      modal.querySelector("#go-to-career").style.display = 'block';
+
+      modal.querySelector("#go-to-career").addEventListener("click", () => {
+        window.location.replace(`careers?query=${finalResult.area}`);
+
+      })
+
+    });
+  }
 }
 
 
 
-const area = (array) => {
+const area = (array, pregNC) => {
   console.log(array)
   let result = 0;
   array.forEach((question) => {
@@ -55,6 +73,9 @@ const area = (array) => {
     let nameQ = question.querySelector("input").name;
     let valueQ = document.querySelector(`input[name=${nameQ}]:checked`)?.value;
 
+    if (!valueQ) {
+      pregNC.push(nameQ)
+    }
     console.log(result, "RESULT!")
     console.log(valueQ, "resultado")
     result += parseInt(valueQ);
